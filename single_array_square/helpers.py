0afd373496db6d94d703924531fb65368307f146
Kurt Ma. Coll <kurt.ma.coll@gmail.com>
2021-02-19 18:58:06 +0800

    Add helper functions

---
+++ single_array_square/helpers.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+def compose (*func):
+    def compose_pair(f, g):
+        return lambda x : f(g(x))
+    return reduce(compose_pair, func, lambda x: x)
+
+def space_leftpad_number (number, width):
+    return str(number).rjust(width, ' ')
